b4dd551eada835260f69632bcca429e434f0da31
Jack Barrett <jbarrett1347@gmail.com>
2021-12-02 14:49:55 -0600

    Added PetOwner put

--- Controllers/PetOwnersController.cs
+++ Controllers/PetOwnersController.cs
@@ -12,15 +12,32 @@ namespace pet_hotel.Controllers
     public class PetOwnersController : ControllerBase
     {
         private readonly ApplicationContext _context;
-        public PetOwnersController(ApplicationContext context) {
+        public PetOwnersController(ApplicationContext context)
+        {
             _context = context;
         }
 
         // This is just a stub for GET / to prevent any weird frontend errors that 
         // occur when the route is missing in this controller
         [HttpGet]
-        public IEnumerable<PetOwner> GetPets() {
+        public IEnumerable<PetOwner> GetPets()
+        {
             return new List<PetOwner>();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, PetOwner petOwner)
+        {
+            Console.WriteLine("In PetOwner PUT");
+
+            if (id != petOwner.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Update(petOwner);
+            _context.SaveChanges();
+            return NoContent();
         }
     }
 }
