c4226b88e67557d3911897989c22390394182cbf
Rob Cone <robcone86@gmail.com>
2021-12-03 09:29:15 -0600

    yeeeyah

--- Controllers/PetsController.cs
+++ Controllers/PetsController.cs
@@ -15,15 +15,18 @@ namespace pet_hotel.Controllers
     public class PetsController : ControllerBase
     {
         private readonly ApplicationContext _context;
-        public PetsController(ApplicationContext context) {
+        public PetsController(ApplicationContext context)
+        {
             _context = context;
         }
 
         // This is just a stub for GET / to prevent any weird frontend errors that 
         // occur when the route is missing in this controller
         [HttpGet]
-        public IEnumerable<Pet> GetPets() {
-            return new List<Pet>();
+        public IEnumerable<Pet> GetPets()
+        {
+            return _context.Pets;
+
         }
 
         // [HttpGet]
